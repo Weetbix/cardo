770386340f530d5b39f8de83146a6044108a55cf
John Hannagan <weetbix_@hotmail.com>
2020-03-29 16:08:52 +0200

    add aws config to git ignore

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./app.json";
+import API from "@aws-amplify/api";
+import amplifyConfig from "./aws-exports";
 
+API.configure(amplifyConfig);
 AppRegistry.registerComponent(appName, () => App);
--- packages/app/src/pages/StartPage.tsx
+++ packages/app/src/pages/StartPage.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Connect } from "aws-amplify-react-native";
+import { graphqlOperation } from "aws-amplify";
+import * as queries from "@cardo/backend/src/graphql/queries";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Connect query={graphqlOperation(queries.listCategorys)}>
+        {({ data, loading, errors }) => {
+          if (errors) return <Text>Error</Text>;
+          if (loading) return <Text>Loading...</Text>;
+          return <Text>{JSON.stringify({ data: data, cat: 4 })}</Text>;
+        }}
+      </Connect>
     </View>
   );
 }
