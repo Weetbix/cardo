6551071c51f6ea3523ed010f1c1bbeb0306c2a21
John Hannagan <weetbix_@hotmail.com>
2020-03-29 22:09:25 +0200

    remove unused import

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import StartPage from "./src/pages/StartPage";
 import CategoryPage from "./src/pages/CategoryPage";
-import CategoryCard from "./src/components/CategoryCard";
 
 const Stack = createStackNavigator<NavStackParamList>();
 
