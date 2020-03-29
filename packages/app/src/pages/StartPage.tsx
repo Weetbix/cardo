55863403eb2a3f2974ab8ede518a5b45e999226c
John Hannagan <weetbix_@hotmail.com>
2020-03-29 17:08:53 +0200

    Show query in start page

--- packages/app/src/pages/StartPage.tsx
+++ packages/app/src/pages/StartPage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Connect } from "aws-amplify-react-native";
-import { graphqlOperation } from "aws-amplify";
+import API, { graphqlOperation } from "@aws-amplify/api";
 import * as queries from "@cardo/backend/src/graphql/queries";
+
+import amplifyConfig from "../../aws-exports.js";
+API.configure(amplifyConfig);
 
 export default function App() {
   return (
@@ -10,7 +13,8 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <Connect query={graphqlOperation(queries.listCategorys)}>
         {({ data, loading, errors }) => {
-          if (errors) return <Text>Error</Text>;
+          if (errors && errors.length)
+            return <Text>d {JSON.stringify(errors)}</Text>;
           if (loading) return <Text>Loading...</Text>;
           return <Text>{JSON.stringify({ data: data, cat: 4 })}</Text>;
         }}
