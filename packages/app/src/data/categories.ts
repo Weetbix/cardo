c896b58c1519762e9fac2374e22f0f0d81ec9fcb
John Hannagan <weetbix_@hotmail.com>
2020-03-30 21:36:09 +0200

    Add images for other categories

--- packages/app/src/data/categories.ts
+++ packages/app/src/data/categories.ts
@@ -1,8 +1,5 @@
-// import img1 from
-
 import { ImageSourcePropType } from "react-native";
 
-// import { Image } from "react-native";
 export type Category = {
   id: string;
   name: string;
@@ -10,16 +7,12 @@ export type Category = {
   image: ImageSourcePropType;
 };
 
-// let a = require("./images/icon-birthday.png");
-
-// Image;
-
 export const CATEGORIES: Array<Category> = [
   {
     id: "f4ca5ff3-cffc-4c94-865e-073812844270",
     name: "Christmas",
     color: "#F2999E",
-    image: require("./images/icon-birthday.png")
+    image: require("./images/icon-christmas.png")
   },
   {
     id: "cd50c3b0-3c43-431d-9341-a44cc50a6881",
@@ -31,6 +24,6 @@ export const CATEGORIES: Array<Category> = [
     id: "5de7c3b6-8b61-4442-8691-314c2844ed98",
     name: "Get well soon!",
     color: "#B9A8EE",
-    image: require("./images/icon-birthday.png")
+    image: require("./images/icon-getwellsoon.png")
   }
 ];
