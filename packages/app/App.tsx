6880f2b6c3da48f9bb6e2d6e70836d0d475085ef
John Hannagan <weetbix_@hotmail.com>
2020-03-30 20:44:33 +0200

    improve header styles

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  StackNavigationOptions
+} from "@react-navigation/stack";
 import StartPage from "./src/pages/StartPage";
 import CategoryPage from "./src/pages/CategoryPage";
 
 const Stack = createStackNavigator<NavStackParamList>();
 
+const headerStyles: StackNavigationOptions = {
+  headerStyle: {
+    elevation: 0,
+    borderBottomWidth: 0
+  },
+  headerTitleStyle: {
+    fontWeight: "normal",
+    fontFamily: "sans-serif-light"
+  },
+  headerTitleAlign: "center",
+  headerBackImage: () => (
+    <Ionicons name="ios-arrow-back" size={20} style={{ marginLeft: 10 }} />
+  )
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={headerStyles}>
         <Stack.Screen
           name="home"
           component={StartPage}
