ff8f05aac158c12bd339cb1c16b78ebdc08fe34b
John Hannagan <weetbix_@hotmail.com>
2020-04-16 07:06:51 +0200

    Add CRA starter

---
+++ packages/admin-ui/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import "@aws-amplify/ui/dist/style.css";
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App, true);
