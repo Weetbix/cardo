a8c9c9e515c72273fba6c5a45c7a7d9b3c12144b
John Hannagan <weetbix_@hotmail.com>
2020-04-16 20:30:26 +0200

    remove CRA boilerplate

--- packages/admin-ui/src/App.js
+++ packages/admin-ui/src/App.js
@@ -3,31 +3,11 @@ import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
 
-import logo from "./logo.svg";
-import "./App.css";
-
 import "@aws-amplify/ui/dist/style.css";
 Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">ok</div>;
 }
 
 export default withAuthenticator(App, true);
