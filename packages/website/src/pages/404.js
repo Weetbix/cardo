d3ca3d7b34a93b619591698401a135bfa2a4f0cc
John Hannagan <weetbix_@hotmail.com>
2020-04-25 15:38:39 +0200

    Improve 404 page

--- packages/website/src/pages/404.js
+++ packages/website/src/pages/404.js
@@ -1,13 +1,37 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import construction from "../images/construction.svg"
+import styled from "styled-components"
+
+const Content = styled.div`
+  background-color: white;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  img {
+    margin-bottom: 30px;
+    width: 400px;
+  }
+
+  h1 {
+    margin-bottom: 20px;
+  }
+`
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Content>
+      <img src={construction} />
+      <h1>Oh No!</h1>
+      <p>That page doesn't seem to exist</p>
+    </Content>
   </Layout>
 )
 
